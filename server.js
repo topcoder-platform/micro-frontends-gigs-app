20b19296a370204a03409a46fc9a77e69bf9a35f
aT <atif.siddiqui2@topcoder.com>
2021-09-06 12:27:24 +0530

    Change path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-app.use('/gigs',
+app.use('/gigs-app',
   express.static("./dist", {
     setHeaders: function setHeaders(res) {
       res.header("Access-Control-Allow-Origin", "*");
